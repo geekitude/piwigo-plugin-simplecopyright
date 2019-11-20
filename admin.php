fb840facecd4a561ff467ef2d2398a319f34763f
7tonin <7tonin@laposte.net>
2019-11-20 21:04:06 +0100

    Update admin.php

--- admin.php
+++ admin.php
@@ -12,13 +12,9 @@
             $simplecr_url = "https://creativecommons.org/licenses/by/4.0/";
             $simplecr_descr = "You are free to share (copy and redistribute the material in any medium or format) or adapt (remix, transform, and build upon the material) for any purpose, even commercially but must give appropriate credit.";
         } elseif ($_POST['select'] == "by-sa") {
-            $simplecr_label = "CC Attribution-ShareAlike 4.0 International";
-            $simplecr_url = "https://creativecommons.org/licenses/by-sa/4.0/";
-            $simplecr_descr = "You are free to share (copy and redistribute the material in any medium or format) or adapt (remix, transform, and build upon the material) for any purpose, even commercially but must give appropriate credit. If you remix, transform, or build upon the material, you must distribute your contributions under the same license as the original.";
-        } elseif ($_POST['select'] == "by-nd") {
-            $simplecr_label = "CC Attribution-NoDerivatives 4.0 International";
-            $simplecr_url = "https://creativecommons.org/licenses/by-nd/4.0/";
-            $simplecr_descr = "You are free to share (copy and redistribute the material in any medium or format) for any purpose, even commercially. If you remix, transform, or build upon the material, you may not distribute the modified material.";
+            $simplecr_label = l10n('label_by-sa'); 
+            $simplecr_url = l10n('url_by-sa'); 
+            $simplecr_descr = l10n('descr_by-sa');
         } elseif ($_POST['select'] == "by-nc") {
             $simplecr_label = "CC Attribution-NonCommercial 4.0 International";
             $simplecr_url = "https://creativecommons.org/licenses/by-nc/4.0/";
