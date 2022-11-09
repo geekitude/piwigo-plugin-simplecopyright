7f346abb29b628cdfb2c9791585da28b29ddd700
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-11-09 10:55:44 +0100

    Update nb_NO, thanks to paulen

---
+++ language/nb_NO/plugin.lang.php
@@ -0,0 +1,67 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['label_by-nc'] = 'CC Attribusjon-Ikke-kommersielt 4.0 Internasjonal';
+$lang['label_by-nc-nd'] = 'CC Attribusjon-Ikke-kommersielt-Ingen derivater 4.0 Internasjonal';
+$lang['label_by-nc-sa'] = 'CC Attribusjon-Ikke-kommersielt-Del på lik linje 4.0 Internasjonal';
+$lang['label_by-nd'] = 'CC Attribusjon-Ingen derivater 4.0 Internasjonal';
+$lang['label_by-sa'] = 'CC Attribusjon-Del på lik linje 4.0 Internasjonal';
+$lang['label_cc'] = 'CC0 1.0 Universal';
+$lang['label_no-license'] = 'Alle rettigheter forbeholdt';
+$lang['see below'] = 'se nedenfor';
+$lang['the name of the copyright owner, an abbreviation by which the name can be recognized, or a notoriously known alternative designation of owner'] = 'navnet på opphavsrettseieren, en forkortelse som navnet kan gjenkjennes med, eller en notorisk kjent alternativ betegnelse på eieren';
+$lang['the year of first publication'] = 'året for første utgivelse';
+$lang['the © symbol (C letter in a circle), the word “Copyright”, or, the abbreviation “Copr.”'] = '©-symbolet (C-bokstav i en sirkel), ordet "Copyright", eller forkortelsen "Copr."';
+$lang['this Wikipedia article'] = 'denne Wikipedia-artikkelen';
+$lang['url_by'] = 'https://creativecommons.org/licenses/by/4.0/';
+$lang['url_by-nc'] = 'https://creativecommons.org/licenses/by-nc/4.0/';
+$lang['url_by-nc-nd'] = 'https://creativecommons.org/licenses/by-nc-nd/4.0/';
+$lang['url_by-nc-sa'] = 'https://creativecommons.org/licenses/by-nc-sa/4.0/';
+$lang['url_by-nd'] = 'https://creativecommons.org/licenses/by-nd/4.0/';
+$lang['url_by-sa'] = 'https://creativecommons.org/licenses/by-sa/4.0/';
+$lang['url_cc'] = 'https://creativecommons.org/publicdomain/zero/1.0/deed.en';
+$lang['url_no-license'] = 'https://en.wikipedia.org/wiki/All_rights_reserved';
+$lang['License'] = 'Lisens';
+$lang['No license'] = 'Ingen lisens';
+$lang['Note that displaying a copyright and/or a license on a web page isn\'t as legally efficient as adding them in picture\'s metadatas, it only makes them more visible.'] = 'Merk at det å vise en opphavsrett og/eller en lisens på en nettside ikke er like effektivt som å legge dem til i bildets metadata, det gjør dem bare mer synlige.';
+$lang['Note that the abbreviation might not be acceptable in some countries'] = 'Merk at forkortelsen kanskje ikke er akseptabel i enkelte land';
+$lang['Short description:'] = 'Kort beskrivelse:';
+$lang['Summary:'] = 'Sammendrag:';
+$lang['This option seems to greatly improve images SEO but keep in mind that it offers a link to download original image.'] = 'Dette alternativet ser ut til å forbedre SEO for bilder betraktelig, men husk at det tilbyr en lenke for å laste ned originalbildet.';
+$lang['Turn recognizable license label in Copyright metadata into a link to that license details'] = 'Gjør om gjenkjennelig lisensetikett i metadata om opphavsrett til en lenke til lisensdetaljene';
+$lang['URL:'] = 'URL:';
+$lang['Use original image URL instead of page URL for link\'s `about` property'] = 'Bruk originalbilde-URL i stedet for side-URL for koblingens `om`-eierforhold';
+$lang['Will be shown as a tooltip when hovering license link'] = 'Vil bli vist som et verktøytips når du beveger pilen over lisenslenken';
+$lang['descr_by'] = 'Du står fritt til å [DELE] (kopiere og redistribuere materialet i hvilket som helst medium eller format) eller [TILPASSE] (remikse, transformere og bygge videre på materialet) for ethvert formål, selv [KOMMERSIELLT], men må gi passende [KREDITT].';
+$lang['descr_by-nc'] = 'Du står fritt til å [DELE] (kopiere og redistribuere materialet i hvilket som helst medium eller format) eller [TILPASSE] (remikse, transformere og bygge videre på materialet) for alle [IKKE-KOMMERSIELLE] formål, men må gi passende [KREDITT].';
+$lang['descr_by-nc-nd'] = 'Du står fritt til å [DELE] (kopiere og redistribuere materialet i ethvert medium eller format) for alle [IKKE-KOMMERSIELLE] formål, men må gi passende [KREDITT]. Hvis du remikser, transformerer eller bygger på materialet, [KAN DU IKKE DISTRIBUTERE DET MODIFISEREDE MATERIALET].';
+$lang['descr_by-nc-sa'] = 'Du står fritt til å [DELE] (kopiere og redistribuere materialet i hvilket som helst medium eller format) eller [TILPASSE] (remikse, transformere og bygge videre på materialet) for alle [IKKE-KOMMERSIELLE] formål, men må gi passende [KREDITT]. Hvis du remikser, transformerer eller bygger på materialet, må du distribuere bidragene dine [UNDER SAMME LISENS] som originalen.';
+$lang['descr_by-nd'] = 'Du står fritt til å [DELE] (kopiere og redistribuere materialet i ethvert medium eller format) for ethvert formål, selv [KOMMERSIELLT]. Hvis du remikser, transformerer eller bygger på materialet, [KAN DU IKKE DISTRIBUTERE DET MODIFISEREDE MATERIALET].';
+$lang['descr_by-sa'] = 'Du står fritt til å [DELE] (kopiere og redistribuere materialet i hvilket som helst medium eller format) eller [TILPASSE] (remikse, transformere og bygge videre på materialet) for ethvert formål, selv [KOMMERSIELLT], men må gi passende [KREDITT]. Hvis du remikser, transformerer eller bygger på materialet, må du distribuere bidragene dine [UNDER SAMME LISENS] som originalen.';
+$lang['descr_cc'] = 'Du kan [KOPIERE], [ENDRE], [DISTRIBUTERE] og [UTFØRE] arbeidet, selv for [KOMMERSIELLE] formål, alt uten å spørre om tillatelse.';
+$lang['descr_no-license'] = 'Det er ingen lisens som gir deg noen rett til å gjenbruke noe materiale fra denne nettsiden på noen måte, se opphavsrett. Merk at formelen "Alle rettigheter forbeholdt" ikke har noen juridisk verdi igjen i noen juridiksjon, men brukes her for å forhindre tvetydighet.';
+$lang['label_by'] = 'CC Attribusjon 4.0 Internasjonal';
+$lang['About copyright notice'] = 'Om opphavsrettserklæring';
+$lang['All Rights Reserved'] = 'Alle rettigheter forbeholdt';
+$lang['Any license requires a link to a full description to be of any value'] = 'Enhver lisens krever en lenke til en fullstendig beskrivelse for å ha noen verdi';
+$lang['Choice:'] = 'Valg';
+$lang['Copyright'] = 'opphavsrett';
+$lang['Custom license'] = 'Tilpasset lisens';
+$lang['Custom license label shown by Piwigo'] = 'Egendefinert lisensetikett vist av Piwigo';
+$lang['Default license'] = 'Standard lisens';
+$lang['Display default license in site footer'] = 'Vis standardlisens i bunnteksten på nettstedet';
+$lang['Display image page Copyright section'] = 'Vis bildeside opphavsrett-seksjonen';
+$lang['Elements legally required to correctly present a Copyright in most countries:'] = 'Elementer som er lovpålagt for å presentere en opphavsrett korrekt i de fleste land:';
+$lang['Examples of recognizable license labels:'] = 'Eksempler på gjenkjennelige lisensetiketter:';
+$lang['Except where otherwise noted, content on this website is licensed under the following license:'] = 'Med mindre annet er angitt, er innholdet på denne nettsiden lisensiert under følgende lisens:';
+$lang['Except where otherwise noted, there is no license granted on this website\'s content, which means it is protected by corresponding copyright'] = 'Med mindre annet er angitt, er det ingen lisens gitt på dette nettstedets innhold, noe som betyr at det er beskyttet av tilsvarende opphavsrett';
+$lang['Excerpt from'] = 'Utdrag fra';
+$lang['If image has no Copyright info, display default license'] = 'Hvis bildet ikke har informasjon om opphavsrett, vis standard lisens';
+$lang['Image does not contain any Copyright, it would be wise to add one in metadata.'] = 'Bildet inneholder ingen opphavsrett, det ville være lurt å legge til en i metadata.';
+$lang['Image pages'] = 'Sider med bilder';
+$lang['It is possible and often advised (or even required in some countries) to add the licence after the Copyright, here come a few examples:'] = 'Det er mulig og ofte anbefalt (eller til og med nødvendig i enkelte land) å legge til lisensen etter opphavsretten, her kommer noen eksempler:';
+$lang['Label:'] = 'Merkelapp:';
