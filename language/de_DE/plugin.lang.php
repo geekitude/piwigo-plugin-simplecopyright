6d3a715bee2aa483f16efae0005ba9b4d67d740f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-11-09 20:18:04 +0100

    Update de_DE, thanks to slim

---
+++ language/de_DE/plugin.lang.php
@@ -0,0 +1,68 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['About copyright notice'] = 'Über den Copyright-Hinweis';
+$lang['All Rights Reserved'] = 'Alle Rechte vorbehalten';
+$lang['Custom license label shown by Piwigo'] = 'Benutzerdefiniertes Lizenzlabel, angezeigt von Piwigo';
+$lang['Display default license in site footer'] = 'Standardlizenz in der Fußzeile der Website anzeigen';
+$lang['Examples of recognizable license labels:'] = 'Beispiele für erkennbare Lizenzkennzeichnungen:';
+$lang['url_by-nd'] = 'https://creativecommons.org/licenses/by-nd/4.0/';
+$lang['url_by-sa'] = 'https://creativecommons.org/licenses/by-sa/4.0/';
+$lang['url_no-license'] = 'https://en.wikipedia.org/wiki/All_rights_reserved';
+$lang['This option seems to greatly improve images SEO but keep in mind that it offers a link to download original image.'] = 'Diese Option scheint die Bild-SEO stark zu verbessern, aber bedenken Sie, dass sie einen Link zum Herunterladen des Originalbildes anbietet.';
+$lang['Turn recognizable license label in Copyright metadata into a link to that license details'] = 'Erkennbare Lizenzbezeichnung in den Copyright-Metadaten in einen Link zu den Lizenzdetails umwandeln';
+$lang['Note that the abbreviation might not be acceptable in some countries'] = 'Beachten Sie, dass die Abkürzung in einigen Ländern möglicherweise nicht zulässig ist';
+$lang['descr_by'] = 'Es steht Ihnen frei, das Material zu [TEILEN] (Kopieren und Weitergeben des Materials in jedem Medium oder Format) oder [ANZUPASSEN] (Remixen, Umwandeln und Aufbauen auf dem Material) für jeden Zweck, auch [GEWERBLICH], aber Sie müssen einen angemessenen [DANK] geben.';
+$lang['descr_by-nc'] = 'Es steht Ihnen frei, das Material in jedem Medium oder Format zu [TEILEN] (zu kopieren und weiterzugeben) oder [ANZUPASSEN] (neu zu mischen, umzuwandeln und auf dem Material aufzubauen) für jeden Zweck, auch [NICHT-KOMMERZIELL], aber Sie müssen einen angemessenen [CREDIT] angeben.';
+$lang['descr_cc'] = 'Sie können das Werk [KOPIEREN], [VERÄNDERN], [VERTEILEN] und [AUFFÜHREN], sogar für [GEWERBLICHE] Zwecke, ohne um Erlaubnis zu fragen.';
+$lang['label_by'] = 'CC Namensnennung 4.0 International';
+$lang['the year of first publication'] = 'das Jahr der Erstveröffentlichung';
+$lang['the © symbol (C letter in a circle), the word “Copyright”, or, the abbreviation “Copr.”'] = 'das ©-Symbol (Buchstabe C in einem Kreis), das Wort "Copyright" oder die Abkürzung "Copr."';
+$lang['this Wikipedia article'] = 'diesen Wikipedia-Artikel';
+$lang['url_by'] = 'https://creativecommons.org/licenses/by/4.0/';
+$lang['url_by-nc'] = 'https://creativecommons.org/licenses/by-nc/4.0/';
+$lang['url_by-nc-nd'] = 'https://creativecommons.org/licenses/by-nc-nd/4.0/';
+$lang['url_by-nc-sa'] = 'https://creativecommons.org/licenses/by-nc-sa/4.0/';
+$lang['url_cc'] = 'https://creativecommons.org/publicdomain/zero/1.0/deed.en';
+$lang['label_by-nc'] = 'CC Namensnennung-NichtKommerziell 4.0 International';
+$lang['label_by-nc-nd'] = 'CC Namensnennung-NichtKommerziell-KeineDerivate 4.0 International';
+$lang['label_by-nc-sa'] = 'CC Namensnennung-NichtKommerziell-Weitergabe unter gleichen Bedingungen 4.0 International
+';
+$lang['label_by-nd'] = 'CC Namensnennung-KeineDerivate 4.0 International';
+$lang['label_by-sa'] = 'CC Namensnennung-Weitergabe unter gleichen Bedingungen 4.0 International';
+$lang['label_cc'] = 'CC0 1.0 Universal';
+$lang['label_no-license'] = 'Alle Rechte vorbehalten';
+$lang['see below'] = 'siehe unten';
+$lang['the name of the copyright owner, an abbreviation by which the name can be recognized, or a notoriously known alternative designation of owner'] = 'den Namen des Urheberrechtsinhabers, eine Abkürzung, durch die der Name erkannt werden kann, oder eine notorisch bekannte alternative Bezeichnung des Inhabers';
+$lang['Use original image URL instead of page URL for link\'s `about` property'] = 'Original-URL des Bildes anstelle der Seiten-URL für die Eigenschaft "about" des Links verwenden';
+$lang['Will be shown as a tooltip when hovering license link'] = 'Wird als Tooltip angezeigt, wenn der Mauszeiger über den Lizenzlink bewegt wird';
+$lang['descr_by-nc-nd'] = 'Es steht Ihnen frei, das Material für jeden [NICHTKOMMERZIELLEN] Zweck zu [TEILEN] (das Material in jedem Medium oder Format zu kopieren und weiterzugeben), wobei jedoch ein angemessener [CREDIT] anzugeben ist. Wenn Sie das Material remixen, umwandeln oder darauf aufbauen, [DÜRFEN SIE DAS VERÄNDERTE MATERIAL NICHT VERTREIBEN].';
+$lang['descr_by-nc-sa'] = 'Es steht Ihnen frei, das Material in jedem Medium oder Format zu [TEILEN] (zu kopieren und weiterzugeben) oder [ANZUPASSEN] (das Material zu remixen, umzuwandeln und darauf aufzubauen) für jeden [NICHTKOMMERZIELLEN] Zweck, aber Sie müssen einen entsprechenden [CREDIT] angeben. Wenn Sie das Material remixen, umwandeln oder darauf aufbauen, müssen Sie Ihre Beiträge [UNTER DER GLEICHEN LIZENZ] wie das Original verbreiten.';
+$lang['descr_by-nd'] = 'Es steht Ihnen frei, [SHARE] (das Material auf einem beliebigen Medium oder in einem beliebigen Format zu vervielfältigen und weiterzugeben) für jeden Zweck, auch [COMMERCIALLY]. Wenn Sie das Material remixen, umwandeln oder darauf aufbauen, [DÜRFEN SIE DAS VERÄNDERTE MATERIAL NICHT VERTREIBEN].';
+$lang['descr_by-sa'] = 'Es steht Ihnen frei, das Material zu [TEILEN] (Kopieren und Weiterverbreiten des Materials in einem beliebigen Medium oder Format) oder [ANZUPASSEN] (Remixen, Umwandeln und Aufbauen auf dem Material) für beliebige Zwecke, auch [GEWERBLICH], zu verwenden, aber Sie müssen angemessene [ANERKENNUNG] geben. Wenn Sie das Material remixen, umwandeln oder darauf aufbauen, müssen Sie Ihre Beiträge [UNTER DER GLEICHEN LIZENZ] wie das Original verbreiten.';
+$lang['descr_no-license'] = 'Es gibt keine Lizenz, die Ihnen das Recht einräumt, Material von dieser Website in irgendeiner Weise wiederzuverwenden, siehe Urheberrechte. Beachten Sie, dass die Formulierung "Alle Rechte vorbehalten" in keiner Rechtsordnung einen rechtlichen Wert hat, sondern hier verwendet wird, um Unklarheiten zu vermeiden.';
+$lang['Excerpt from'] = 'Auszug aus';
+$lang['If image has no Copyright info, display default license'] = 'Wenn Bild keine Copyright-Informationen hat, Standardlizenz anzeigen';
+$lang['Image does not contain any Copyright, it would be wise to add one in metadata.'] = 'Das Bild enthält kein Copyright, es wäre ratsam, eines in den Metadaten hinzuzufügen.';
+$lang['Image pages'] = 'Seiten der Bilder';
+$lang['It is possible and often advised (or even required in some countries) to add the licence after the Copyright, here come a few examples:'] = 'Es ist möglich und oft ratsam (oder in einigen Ländern sogar vorgeschrieben), die Lizenz nach dem Copyright hinzuzufügen, hier ein paar Beispiele:';
+$lang['Label:'] = 'Etikett:';
+$lang['License'] = 'Lizenz';
+$lang['No license'] = 'Keine Lizenz';
+$lang['Note that displaying a copyright and/or a license on a web page isn\'t as legally efficient as adding them in picture\'s metadatas, it only makes them more visible.'] = 'Beachten Sie, dass die Anzeige eines Copyrights und/oder einer Lizenz auf einer Webseite nicht so effizient ist wie das Hinzufügen in den Metadaten eines Bildes, sie macht sie nur besser sichtbar.';
+$lang['Short description:'] = 'Kurze Beschreibung:';
+$lang['Summary:'] = 'Zusammenfassung:';
+$lang['URL:'] = 'URL:';
+$lang['Any license requires a link to a full description to be of any value'] = 'Jede Lizenz erfordert einen Link zu einer vollständigen Beschreibung, um von Wert zu sein';
+$lang['Choice:'] = 'Auswählen:';
+$lang['Copyright'] = 'Urheberrecht';
+$lang['Custom license'] = 'Benutzerdefinierte Lizenz';
+$lang['Default license'] = 'Standard-Lizenz';
+$lang['Display image page Copyright section'] = 'Bildseite anzeigen Abschnitt Copyright';
+$lang['Elements legally required to correctly present a Copyright in most countries:'] = 'Elemente, die in den meisten Ländern gesetzlich vorgeschrieben sind, um ein Copyright korrekt darzustellen:';
+$lang['Except where otherwise noted, content on this website is licensed under the following license:'] = 'Sofern nicht anders angegeben, ist der Inhalt dieser Website unter der folgenden Lizenz lizenziert:';
+$lang['Except where otherwise noted, there is no license granted on this website\'s content, which means it is protected by corresponding copyright'] = 'Sofern nicht anders angegeben, wird für den Inhalt dieser Website keine Lizenz erteilt, d.h. er ist durch das entsprechende Urheberrecht geschützt.';
