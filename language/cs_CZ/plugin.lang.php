ba5f55049d3808b61fbb0b7091e7760f11868e4c
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-11-09 20:13:44 +0100

    Update cs_CZ, thanks to MiPi

---
+++ language/cs_CZ/plugin.lang.php
@@ -0,0 +1,37 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Will be shown as a tooltip when hovering license link'] = 'Zobrazí se jako nápověda (tooltip) při najetí myší na odkaz na licenci';
+$lang['Custom license label shown by Piwigo'] = 'Štítek vlastní licence co zobrazí Piwigo';
+$lang['Examples of recognizable license labels:'] = 'Příklady rozpoznatelných licenčních štítků:';
+$lang['Label:'] = 'Štítek:';
+$lang['License'] = 'Licence';
+$lang['No license'] = 'Žádná licence';
+$lang['Note that displaying a copyright and/or a license on a web page isn\'t as legally efficient as adding them in picture\'s metadatas, it only makes them more visible.'] = 'Všimněte si, že zobrazení autorských práv a/nebo licence na webové stránce není tak účinné jako jejich přidání do metadat obrázku, pouze je zviditelňuje.';
+$lang['Note that the abbreviation might not be acceptable in some countries'] = 'Upozorňujeme, že v některých zemích nemusí být tato zkratka akceptovaná';
+$lang['Short description:'] = 'Krátký popis:';
+$lang['Summary:'] = 'Souhrn:';
+$lang['This option seems to greatly improve images SEO but keep in mind that it offers a link to download original image.'] = 'Tato možnost zřejmě výrazně zlepšuje SEO obrázků, ale mějte na paměti, že nabízí odkaz na stažení původního obrázku.';
+$lang['Turn recognizable license label in Copyright metadata into a link to that license details'] = 'Nahradit rozpoznatelný licenční štítek v metadatech o copyrightu na odkaz na podrobnosti o licenci';
+$lang['URL:'] = 'URL:';
+$lang['Image pages'] = 'Stránky obrázků';
+$lang['It is possible and often advised (or even required in some countries) to add the licence after the Copyright, here come a few examples:'] = 'Je možné a často se doporučuje (nebo v některých zemích dokonce vyžaduje) přidat za copyright licenci, zde je několik příkladů:';
+$lang['Image does not contain any Copyright, it would be wise to add one in metadata.'] = 'Obrázek neobsahuje žádný copyright, bylo by vhodné ho přidat do metadat.';
+$lang['Default license'] = 'Výchozí licence';
+$lang['Display default license in site footer'] = 'Zobrazit výchozí licenci v patičce stránky';
+$lang['Except where otherwise noted, there is no license granted on this website\'s content, which means it is protected by corresponding copyright'] = 'Pokud není uvedeno jinak, není na obsah těchto webových stránek poskytnuta žádná licence, což znamená, že je chráněn příslušnými autorskými právy';
+$lang['Excerpt from'] = 'Ukázka z';
+$lang['If image has no Copyright info, display default license'] = 'Pokud obrázek nemá informaci o autorských právech, zobrazit výchozí licenci';
+$lang['Display image page Copyright section'] = 'Zobrazit sekci pro copyright na stránce obrázku';
+$lang['Elements legally required to correctly present a Copyright in most countries:'] = 'Prvky, které jsou ze zákona vyžadovány pro správnou prezentaci autorských práv ve většině zemí:';
+$lang['Except where otherwise noted, content on this website is licensed under the following license:'] = 'Pokud není uvedeno jinak, je obsah těchto webových stránek licencován pod následující licencí:';
+$lang['About copyright notice'] = 'Oznámení o autorských právech';
+$lang['All Rights Reserved'] = 'Všechna práva vyhrazena';
+$lang['Any license requires a link to a full description to be of any value'] = 'Každá licence vyžaduje odkaz na úplný popis, aby měla nějakou hodnotu';
+$lang['Choice:'] = 'Výběr:';
+$lang['Copyright'] = 'Copyright';
+$lang['Custom license'] = 'Vlastní licence';
