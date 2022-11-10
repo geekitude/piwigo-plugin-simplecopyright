16b19109665b0daccd48cd9ae01f55f1f571e944
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-11-10 13:56:45 +0100

    Update is_IS, thanks to gudjonsi

---
+++ language/is_IS/plugin.lang.php
@@ -0,0 +1,67 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['this Wikipedia article'] = 'þessa Wikipedia grein';
+$lang['url_by'] = 'https://creativecommons.org/licenses/by/4.0/';
+$lang['url_by-nc'] = 'https://creativecommons.org/licenses/by-nc/4.0/';
+$lang['url_by-nc-nd'] = 'https://creativecommons.org/licenses/by-nc-nd/4.0/';
+$lang['url_by-nc-sa'] = 'https://creativecommons.org/licenses/by-nc-sa/4.0/';
+$lang['url_by-nd'] = 'https://creativecommons.org/licenses/by-nd/4.0/';
+$lang['url_by-sa'] = 'https://creativecommons.org/licenses/by-sa/4.0/';
+$lang['url_cc'] = 'https://creativecommons.org/publicdomain/zero/1.0/deed.en';
+$lang['url_no-license'] = 'https://en.wikipedia.org/wiki/All_rights_reserved';
+$lang['descr_by'] = 'Þér er frjálst að [DEILA] (afrita og endurdreifa efninu á hvaða miðli eða sniði sem er) eða [AÐLAGA] (endurblanda, umbreyta og byggja ofan á efnið) í hvaða tilgangi sem er, jafnvel í [VIÐSKIPTA] tilgangi en verður að veita viðeigandi [HRÓS].';
+$lang['descr_by-nc'] = 'Þér er frjálst að [DEILA] (afrita og endurdreifa efninu á hvaða miðli eða sniði sem er) eða [AÐLAGA] (endurblanda, umbreyta og byggja ofan á efnið) í hvaða [EKKI VIÐSKIPTA] tilgangi en verður að veita viðeigandi [HRÓS].';
+$lang['descr_by-nc-nd'] = 'Þér er frjálst að [DEILA] (afrita og endurdreifa efninu á hvaða miðli eða sniði sem er) í hvaða [EKKI VIÐSKIPTA] tilgangi en verður að veita viðeigandi [HRÓS]. Ef þú endurblandar, umbreytir eða byggir ofan á efnið, [MÁTTU EKKI DREIFA BREYTTU EFNI].';
+$lang['descr_by-nc-sa'] = 'Þér er frjálst að [DEILA] (afrita og endurdreifa efninu á hvaða miðli eða sniði sem er) eða [AÐLAGA] (endurblanda, umbreyta og byggja ofan á efnið) í hvaða [EKKI VIÐSKIPTA] tilgangi en verður að veita viðeigandi [HRÓS]. Ef þú endurblandar, umbreytir eða byggir á efninu, verður þú að dreifa framlögum þínum [MEÐ SAMMA LEYFI] og upprunalega.';
+$lang['descr_by-nd'] = 'Þér er frjálst að [DEILA] (afrita og endurdreifa efninu á hvaða miðli eða sniði sem er) í hvaða tilgangi sem er, jafnvel í [VIÐSKIPTA] tilgangi. Ef þú endurblandar, umbreytir eða byggir ofan á efnið, [MÁTTU EKKI DREIFA BREYTTU EFNI].';
+$lang['descr_by-sa'] = 'Þér er frjálst að [DEILA] (afrita og endurdreifa efninu á hvaða miðli eða sniði sem er) eða [AÐLAGA] (endurblanda, umbreyta og byggja ofan á efnið) í hvaða tilgangi sem er, jafnvel í [VIÐSKIPTA] tilgangi en verður að veita viðeigandi [HRÓS]. Ef þú endurblandar, umbreytir eða byggir á efninu, verður þú að dreifa framlögum þínum [MEÐ SAMA LEYFI] og upprunalega.';
+$lang['descr_cc'] = 'Þú getur [AFRITAÐ], [BREYTT], [DREIFT] og [FRAMKVÆMT] verkið, jafnvel í [VIÐSKIPTA] tilgangi, allt án þess að biðja um leyfi.';
+$lang['descr_no-license'] = 'Það er ekkert leyfi sem veitir þér nokkurn rétt til að endurnýta efni af þessari vefsíðu á nokkurn hátt, vísa til höfundarréttar. Athugaðu að „Allur réttur áskilinn“ hefur ekki neitt lagalegt gildi eftir í hvaða lögsögu sem er en er notuð hér til að koma í veg fyrir tvíræðni.';
+$lang['label_by'] = 'CC Attribution 4.0 International';
+$lang['label_by-nc'] = 'CC Attribution-NonCommercial 4.0 International';
+$lang['label_by-nc-nd'] = 'CC Attribution-NonCommercial-NoDerivatives 4.0 International';
+$lang['label_by-nc-sa'] = 'CC Attribution-NonCommercial-ShareAlike 4.0 International';
+$lang['label_by-nd'] = 'CC Attribution-NoDerivatives 4.0 International';
+$lang['label_by-sa'] = 'CC Attribution-ShareAlike 4.0 International';
+$lang['label_cc'] = 'CC0 1.0 Universal';
+$lang['label_no-license'] = 'Allur réttur áskilinn';
+$lang['see below'] = 'sjá að neðan';
+$lang['the name of the copyright owner, an abbreviation by which the name can be recognized, or a notoriously known alternative designation of owner'] = 'nafn eiganda höfundarréttar, skammstöfun sem hægt er að bera kennsl á nafnið, eða alþekkt önnur auðkenning eiganda';
+$lang['the year of first publication'] = 'fyrsta útgáfuárið';
+$lang['the © symbol (C letter in a circle), the word “Copyright”, or, the abbreviation “Copr.”'] = '© táknið (C stafur í hring), orðið „Höfundaréttur“ eða skammstöfunin „Höfr.“';
+$lang['License'] = 'Leyfi';
+$lang['No license'] = 'Ekkert leyfi';
+$lang['Note that displaying a copyright and/or a license on a web page isn\'t as legally efficient as adding them in picture\'s metadatas, it only makes them more visible.'] = 'Athugaðu að það að birta höfundarrétt og/eða leyfi á vefsíðu er ekki eins skilvirkt og að bæta þeim við lýsigögn myndarinnar, það gerir þau aðeins sýnilegri.';
+$lang['Note that the abbreviation might not be acceptable in some countries'] = 'Athugaðu að skammstöfunin gæti ekki verið ásættanleg í sumum löndum';
+$lang['Short description:'] = 'Stutt lýsing:';
+$lang['Summary:'] = 'Samantekt:';
+$lang['This option seems to greatly improve images SEO but keep in mind that it offers a link to download original image.'] = 'Þessi valkostur virðist bæta leitarbestun myndar verulega en hafðu í huga að hann býður upp á tengil til að hlaða niður upprunalegu myndinni.';
+$lang['Turn recognizable license label in Copyright metadata into a link to that license details'] = 'Breyttu auðþekkjanlegu leyfismerki í lýsigögnum höfundarréttar í hlekk á leyfisupplýsingarnar';
+$lang['URL:'] = 'Vefslóð:';
+$lang['Use original image URL instead of page URL for link\'s `about` property'] = 'Notaðu upprunalega vefslóð myndar í stað vefslóðar síðu fyrir eigindin „um“ á tengilinn';
+$lang['Will be shown as a tooltip when hovering license link'] = 'Verður sýnd sem verkfæraleiðbeiningar þegar leyfistengillinn er á sveimi';
+$lang['About copyright notice'] = 'Um tilkynningu höfundarréttar';
+$lang['All Rights Reserved'] = 'Allur réttur áskilinn';
+$lang['Any license requires a link to a full description to be of any value'] = 'Sérhvert leyfi krefst krækju á fulla lýsingu til að vera einhvers virði';
+$lang['Choice:'] = 'Val:';
+$lang['Copyright'] = 'Höfundarréttur';
+$lang['Custom license'] = 'Sérsniðið leyfi';
+$lang['Custom license label shown by Piwigo'] = 'Sérsniðið leyfismerki sem Piwigo sýnir';
+$lang['Default license'] = 'Sjálfgefið leyfi';
+$lang['Display default license in site footer'] = 'Sýna sjálfgefið leyfi í síðufæti';
+$lang['Display image page Copyright section'] = 'Birta myndasíðu Höfundarréttahluta';
+$lang['Elements legally required to correctly present a Copyright in most countries:'] = 'Þættir sem eru lagalega nauðsynlegir til að sýna höfundarrétt á réttan hátt í flestum löndum:';
+$lang['Examples of recognizable license labels:'] = 'Dæmi um auðþekkjanleg leyfismerki:';
+$lang['Except where otherwise noted, content on this website is licensed under the following license:'] = 'Nema annað sé tekið fram er leyfi fyrir efni á þessari vefsíðu samkvæmt eftirfarandi leyfi:';
+$lang['Except where otherwise noted, there is no license granted on this website\'s content, which means it is protected by corresponding copyright'] = 'Nema annað sé tekið fram, er ekkert leyfi veitt á efni þessarar vefsíðu, sem þýðir að það er varið með samsvarandi höfundarrétti';
+$lang['Excerpt from'] = 'Útdráttur úr';
+$lang['If image has no Copyright info, display default license'] = 'Ef myndin hefur engar upplýsingar um höfundarrétt skaltu sýna sjálfgefið leyfi';
+$lang['Image does not contain any Copyright, it would be wise to add one in metadata.'] = 'Myndin inniheldur ekki höfundarrétt, það væri skynsamlegt að bæta því við lýsigögnin.';
+$lang['Image pages'] = 'Síður með myndum';
+$lang['It is possible and often advised (or even required in some countries) to add the licence after the Copyright, here come a few examples:'] = 'Það er mögulegt og oft ráðlagt (eða jafnvel krafist í sumum löndum) að bæta leyfinu við eftir höfundarréttinn, hér koma nokkur dæmi:';
+$lang['Label:'] = 'merki:';
