1035b1fe35dbf66d8b8bbc65ef9ae89ad439f284
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-11-24 18:13:21 +0100

    Update nl_NL, thanks to Pat2309

---
+++ language/nl_NL/plugin.lang.php
@@ -0,0 +1,67 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['This option seems to greatly improve images SEO but keep in mind that it offers a link to download original image.'] = 'Deze optie lijkt de SEO van afbeeldingen sterk te verbeteren, maar vergeet niet dat het een link biedt om de originele afbeelding te downloaden.';
+$lang['Turn recognizable license label in Copyright metadata into a link to that license details'] = 'Maak van herkenbare licentielabels in Copyright metadata een link naar die licentiegegevens';
+$lang['descr_by'] = 'U bent vrij om het materiaal te [DELEN] (kopiëren en herverdelen in elk medium of formaat) of te [AANPASSEN] (remixen, transformeren en voortbouwen op het materiaal) voor elk doel, zelfs [COMMERCIËLE] maar u moet gepaste [KREDIET] geven.';
+$lang['descr_by-nc'] = 'U bent vrij om het materiaal te [DELEN] (kopiëren en herverdelen in elk medium of formaat) of te [AANPASSEN] (remixen, transformeren en voortbouwen op het materiaal) voor elk [NIET-COMMERCIEEL] doel, maar u moet gepaste [KREDIET] geven.';
+$lang['descr_by-nd'] = 'U bent vrij om het materiaal te [DELEN] (kopiëren en herverdelen in elk medium of formaat) voor elk doel, zelfs [COMMERCIEEL]. Als u het materiaal remixt, transformeert of erop voortbouwt, mag u [HET GEMODIFICEERDE MATERIAAL NIET VERDELEN].';
+$lang['descr_by-nc-sa'] = 'U bent vrij om het materiaal te [DELEN] (kopiëren en herverdelen in elk medium of formaat) of te [AANPASSEN] (remixen, transformeren en voortbouwen op het materiaal) voor elk [NIET-COMMERCIEEL] doel, maar u moet gepaste [VERMELDING] geven. Als u het materiaal remixt, transformeert of verder ontwikkelt, moet u uw bijdragen verspreiden [ONDER DEZELFDE LICENTIE] als het origineel.';
+$lang['descr_by-nc-nd'] = 'U bent vrij om het materiaal te [DELEN] (kopiëren en herverdelen in elk medium of formaat) voor elk [NIET-COMMERCIEEL] doel, maar u moet de juiste [KREDIET] geven. Als u het materiaal remixt, transformeert of erop voortbouwt, mag u [HET GEMODIFICEERDE MATERIAAL NIET VERDELEN].';
+$lang['descr_by-sa'] = 'U bent vrij om het materiaal te [DELEN] (kopiëren en herverdelen in elk medium of formaat) of te [AANPASSEN] (remixen, transformeren en voortbouwen op het materiaal) voor elk doel, zelfs [COMMERCIËLE] maar u moet gepaste [EIGENDOM] geven. Als u het materiaal remixt, transformeert of verder ontwikkelt, moet u uw bijdragen verspreiden [ONDER DEZELFDE LICENTIE] als het origineel.';
+$lang['descr_cc'] = 'U mag het werk [COPIEREN], [WIJZIGEN], [VERDELEN] en [UITVOEREN], zelfs voor [COMMERCIËLE] doeleinden, allemaal zonder toestemming te vragen.';
+$lang['descr_no-license'] = 'Er is geen licentie die u het recht geeft materiaal van deze website op enigerlei wijze te hergebruiken, zie auteursrechten. Merk op dat de formule "Alle rechten voorbehouden" in geen enkel rechtsgebied nog enige juridische waarde heeft, maar hier wordt gebruikt om dubbelzinnigheid te voorkomen.';
+$lang['Note that displaying a copyright and/or a license on a web page isn\'t as legally efficient as adding them in picture\'s metadatas, it only makes them more visible.'] = 'Merk op dat het tonen van een auteursrecht en/of een licentie op een webpagina niet zo efficiënt is als het toevoegen ervan in de metadata van de afbeelding, het maakt ze alleen beter zichtbaar.';
+$lang['Note that the abbreviation might not be acceptable in some countries'] = 'Merk op dat de afkorting in sommige landen niet aanvaardbaar is.';
+$lang['Summary:'] = 'Samenvatting:';
+$lang['License'] = 'Licensie';
+$lang['No license'] = 'Geen licentie';
+$lang['Short description:'] = 'Korte beschrijving';
+$lang['URL:'] = 'URL:';
+$lang['Use original image URL instead of page URL for link\'s `about` property'] = 'Gebruik originele afbeelding URL in plaats van pagina URL voor de `over` eigenschap van de link';
+$lang['Will be shown as a tooltip when hovering license link'] = 'Wordt getoond als een tooltip bij het zweven over de licentielink';
+$lang['label_by'] = 'CC Attribution 4.0 International';
+$lang['label_by-nc'] = 'CC Attribution-NonCommercial 4.0 International';
+$lang['label_by-nc-nd'] = 'CC Attribution-NonCommercial-NoDerivatives 4.0 International';
+$lang['label_by-nc-sa'] = 'CC Attribution-NonCommercial-ShareAlike 4.0 International';
+$lang['label_by-nd'] = 'CC Attribution-NoDerivatives 4.0 International';
+$lang['label_by-sa'] = 'CC Attribution-ShareAlike 4.0 International';
+$lang['label_cc'] = 'CC0 1.0 Universal';
+$lang['label_no-license'] = 'Alle rechten voorbehouden';
+$lang['see below'] = 'zie hieronder';
+$lang['the name of the copyright owner, an abbreviation by which the name can be recognized, or a notoriously known alternative designation of owner'] = 'de naam van de eigenaar van het auteursrecht, een afkorting waaraan de naam kan worden herkend, of een algemeen bekende alternatieve aanduiding van de eigenaar';
+$lang['the year of first publication'] = 'het jaar van de eerste publicatie';
+$lang['the © symbol (C letter in a circle), the word “Copyright”, or, the abbreviation “Copr.”'] = 'het ©-teken (C-letter in een cirkel), het woord "Copyright", of de afkorting "Copr.".';
+$lang['this Wikipedia article'] = 'dit Wikipedia artikel';
+$lang['url_by'] = 'https://creativecommons.org/licenses/by/4.0/';
+$lang['url_by-nc'] = 'https://creativecommons.org/licenses/by-nc/4.0/';
+$lang['url_by-nc-nd'] = 'https://creativecommons.org/licenses/by-nc-nd/4.0/';
+$lang['url_by-nc-sa'] = 'https://creativecommons.org/licenses/by-nc-sa/4.0/';
+$lang['url_by-nd'] = 'https://creativecommons.org/licenses/by-nd/4.0/';
+$lang['url_by-sa'] = 'https://creativecommons.org/licenses/by-sa/4.0/';
+$lang['url_cc'] = 'https://creativecommons.org/publicdomain/zero/1.0/deed.en';
+$lang['url_no-license'] = 'https://en.wikipedia.org/wiki/All_rights_reserved';
+$lang['About copyright notice'] = 'Over auteursrecht';
+$lang['All Rights Reserved'] = 'Alle rechten voorbehouden';
+$lang['Any license requires a link to a full description to be of any value'] = 'Elke licentie vereist een link naar een volledige beschrijving om van enige waarde te zijn.';
+$lang['Choice:'] = 'Keuze:';
+$lang['Copyright'] = 'Auteursrecht';
+$lang['Custom license'] = 'Aangepaste licentie';
+$lang['Custom license label shown by Piwigo'] = 'Aangepast licentielabel getoond door Piwigo';
+$lang['Default license'] = 'Standaard licentie';
+$lang['Display default license in site footer'] = 'Toon standaard licentie in voettekst site';
+$lang['Display image page Copyright section'] = 'Afbeelding pagina Copyright sectie weergeven';
+$lang['Elements legally required to correctly present a Copyright in most countries:'] = 'Elementen die wettelijk vereist zijn om een Auteursrecht correct te presenteren in de meeste landen:';
+$lang['Examples of recognizable license labels:'] = 'Voorbeelden van herkenbare licentielabels:';
+$lang['Except where otherwise noted, content on this website is licensed under the following license:'] = 'Tenzij anders vermeld, is de inhoud van deze website gelicentieerd onder de volgende licentie:';
+$lang['Except where otherwise noted, there is no license granted on this website\'s content, which means it is protected by corresponding copyright'] = 'Tenzij anders vermeld, is er geen licentie verleend op de inhoud van deze website, wat betekent dat het wordt beschermd door de overeenkomstige auteursrechten.';
+$lang['Excerpt from'] = 'Fragment uit';
+$lang['If image has no Copyright info, display default license'] = 'Als de afbeelding geen Auteursrecht info heeft, toon standaard licentie';
+$lang['Image does not contain any Copyright, it would be wise to add one in metadata.'] = 'Afbeelding bevat geen Auteursrecht, het zou verstandig zijn er een toe te voegen in de metadata.';
+$lang['Image pages'] = 'Afbeelding pagina\'s';
+$lang['It is possible and often advised (or even required in some countries) to add the licence after the Copyright, here come a few examples:'] = 'Het is mogelijk en vaak aangeraden (of zelfs verplicht in sommige landen) om de licentie toe te voegen na het Auteursrecht, hier volgen enkele voorbeelden:';
+$lang['Label:'] = 'Label:';
