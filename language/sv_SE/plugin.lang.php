3275a6b65de796dcf80e5be077cc61dbfef5db49
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-12-20 08:10:13 +0100

    Update sv_SE, thanks to PBobbenB

---
+++ language/sv_SE/plugin.lang.php
@@ -0,0 +1,67 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['label_by-nc'] = 'CC Attribution-NonCommercial 4.0 International';
+$lang['label_by-nc-nd'] = 'CC Attribution-NonCommercial-NoDerivatives 4.0 International';
+$lang['label_by-nc-sa'] = 'CC Attribution-NonCommercial-ShareAlike 4.0 International';
+$lang['label_by-nd'] = 'CC Attribution-NoDerivatives 4.0 International';
+$lang['label_by-sa'] = 'CC Attribution-ShareAlike 4.0 International';
+$lang['label_cc'] = 'CC0 1.0 Universal';
+$lang['label_no-license'] = 'Alla rättigheter förbehållna';
+$lang['see below'] = 'se nedan';
+$lang['the name of the copyright owner, an abbreviation by which the name can be recognized, or a notoriously known alternative designation of owner'] = 'namnet på upphovsrättsinnehavaren, en förkortning med vilken namnet kan kännas igen, eller en notoriskt känd alternativ beteckning på ägaren';
+$lang['the year of first publication'] = 'första publiceringsåret';
+$lang['the © symbol (C letter in a circle), the word “Copyright”, or, the abbreviation “Copr.”'] = '©-symbolen (bokstav C i en cirkel), ordet "Copyright" eller förkortningen "Copr."';
+$lang['this Wikipedia article'] = 'denna Wikipedia artikel';
+$lang['url_by'] = 'https://creativecommons.org/licenses/by/4.0/';
+$lang['url_by-nc'] = 'https://creativecommons.org/licenses/by-nc/4.0/';
+$lang['url_by-nc-nd'] = 'https://creativecommons.org/licenses/by-nc-nd/4.0/';
+$lang['url_by-nc-sa'] = 'https://creativecommons.org/licenses/by-nc-sa/4.0/';
+$lang['url_by-nd'] = 'https://creativecommons.org/licenses/by-nd/4.0/';
+$lang['url_by-sa'] = 'https://creativecommons.org/licenses/by-sa/4.0/';
+$lang['url_cc'] = 'https://creativecommons.org/publicdomain/zero/1.0/deed.en';
+$lang['url_no-license'] = 'https://en.wikipedia.org/wiki/All_rights_reserved';
+$lang['License'] = 'Licens';
+$lang['No license'] = 'Ingen licens';
+$lang['Note that displaying a copyright and/or a license on a web page isn\'t as legally efficient as adding them in picture\'s metadatas, it only makes them more visible.'] = 'Observera att det inte är lika effektivt att visa en upphovsrätt och/eller en licens på en webbsida som att lägga till dem i bildens metadata, det gör dem bara mer synliga.';
+$lang['Note that the abbreviation might not be acceptable in some countries'] = 'Observera att förkortningen kanske inte är acceptabel i vissa länder';
+$lang['Short description:'] = 'Kort beskrivning:';
+$lang['Summary:'] = 'Sammanfattning:';
+$lang['This option seems to greatly improve images SEO but keep in mind that it offers a link to download original image.'] = 'Det här alternativet verkar avsevärt förbättra bildens SEO men kom ihåg att det erbjuder en länk för att ladda ner originalbilden.';
+$lang['Turn recognizable license label in Copyright metadata into a link to that license details'] = 'Förvandla igenkännbar licensetikett i upphovsrättsmetadata till en länk till den licensinformationen';
+$lang['URL:'] = 'URL:';
+$lang['Use original image URL instead of page URL for link\'s `about` property'] = 'Använd originalbildens URL istället för sidans URL för länkens "om"-egenskap';
+$lang['Will be shown as a tooltip when hovering license link'] = 'Kommer att visas som ett verktygstips när du svävar på licenslänken';
+$lang['descr_by'] = 'Du är fri att [DELA] (kopiera och omdistribuera materialet i vilket medium eller format som helst) eller [ADAPT] (remixa, transformera och bygga vidare på materialet) för vilket syfte som helst, även [KOMMERSIELLT] men måste ge lämplig [KREDIT].';
+$lang['descr_by-nc'] = 'Du är fri att [DELA] (kopiera och omdistribuera materialet i vilket medium eller format som helst) eller [ADAPT] (remixa, transformera och bygga vidare på materialet) för alla [ICKE-KOMMERSIELLA] syften men måste ge lämplig [KREDIT].';
+$lang['descr_by-nc-nd'] = 'Du är fri att [DELA] (kopiera och omdistribuera materialet i vilket medium eller format som helst) för alla [ICKE-KOMMERSIELLA] ändamål men måste ge lämplig [KREDIT]. Om du remixar, transformerar eller bygger vidare på materialet [FÅR du INTE DISTRIBUERA DET MODIFIERADE MATERIALET].';
+$lang['descr_by-nc-sa'] = 'Du är fri att [DELA] (kopiera och omdistribuera materialet i vilket medium eller format som helst) eller [ADAPT] (remixa, transformera och bygga vidare på materialet) för alla [ICKE-KOMMERSIELLA] syften men måste ge lämplig [KREDIT]. Om du remixar, transformerar eller bygger vidare på materialet måste du distribuera dina bidrag [UNDER SAMMA LICENS] som originalet.';
+$lang['descr_by-nd'] = 'Du är fri att [DELA] (kopiera och vidaredistribuera materialet i vilket medium eller format som helst) för alla ändamål, även [KOMMERCIELLT]. Om du remixar, transformerar eller bygger vidare på materialet [FÅR du INTE DISTRIBUERA DET MODIFIERADE MATERIALET].';
+$lang['descr_by-sa'] = 'Du är fri att [DELA] (kopiera och omdistribuera materialet i vilket medium eller format som helst) eller [ADAPT] (remixa, transformera och bygga vidare på materialet) för vilket syfte som helst, även [KOMMERSIELLT] men måste ge lämplig [KREDIT]. Om du remixar, transformerar eller bygger vidare på materialet måste du distribuera dina bidrag [UNDER SAMMA LICENS] som originalet.';
+$lang['descr_cc'] = 'Du kan [KOPIERA], [ÄNDRA], [DISTRIBUERA] och [UTFÖRA] arbetet, även för [KOMMERSIELLA] ändamål, allt utan att fråga om tillstånd.';
+$lang['descr_no-license'] = 'Det finns ingen licens som ger dig någon rätt att återanvända material från denna webbplats på något sätt, se upphovsrätt. Observera att formeln "Alla rättigheter reserverade" inte har något juridiskt värde kvar i någon juridiktion utan används här för att förhindra oklarheter.';
+$lang['label_by'] = 'CC Attribution 4.0 International';
+$lang['About copyright notice'] = 'Om upphovsrättsmeddelande';
+$lang['All Rights Reserved'] = 'Alla rättigheter förbehållna';
+$lang['Any license requires a link to a full description to be of any value'] = 'Varje licens kräver en länk till en fullständig beskrivning för att vara av något värde';
+$lang['Choice:'] = 'Val:';
+$lang['Copyright'] = 'Upphovsrätt';
+$lang['Custom license'] = 'Anpassad licens';
+$lang['Custom license label shown by Piwigo'] = 'Anpassad licensetikett som visas av Piwigo';
+$lang['Default license'] = 'Standardlicens';
+$lang['Display default license in site footer'] = 'Visa standardlicens i sidfoten på webbplatsen';
+$lang['Display image page Copyright section'] = 'Visa bildsida Copyrightsektionen';
+$lang['Elements legally required to correctly present a Copyright in most countries:'] = 'Element som krävs enligt lag för att korrekt presentera en upphovsrätt i de flesta länder:';
+$lang['Examples of recognizable license labels:'] = 'Exempel på igenkännbara licensetiketter:';
+$lang['Except where otherwise noted, content on this website is licensed under the following license:'] = 'Om inte annat anges är innehållet på denna webbplats licensierat under följande licens:';
+$lang['Except where otherwise noted, there is no license granted on this website\'s content, which means it is protected by corresponding copyright'] = 'Förutom där annat anges, finns det ingen licens beviljad på denna webbplats innehåll, vilket innebär att den är skyddad av motsvarande upphovsrätt';
+$lang['Excerpt from'] = 'Utdrag från';
+$lang['If image has no Copyright info, display default license'] = 'Om bilden inte har någon upphovsrättsinformation, visa standardlicens';
+$lang['Image does not contain any Copyright, it would be wise to add one in metadata.'] = 'Bilden innehåller ingen upphovsrätt, det skulle vara klokt att lägga till en i metadata.';
+$lang['Image pages'] = 'Bildsidor';
+$lang['It is possible and often advised (or even required in some countries) to add the licence after the Copyright, here come a few examples:'] = 'Det är möjligt och ofta rekommenderat (eller till och med krävs i vissa länder) att lägga till licensen efter upphovsrätten, här kommer några exempel:';
+$lang['Label:'] = 'Etikett:';
