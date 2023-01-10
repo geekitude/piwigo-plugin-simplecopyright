96a30618f56a05e266545027c467ef801edcf390
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-01-10 06:22:23 +0100

    Update tr_TR, thanks to LazBoy

---
+++ language/tr_TR/plugin.lang.php
@@ -0,0 +1,67 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['the name of the copyright owner, an abbreviation by which the name can be recognized, or a notoriously known alternative designation of owner'] = 'telif hakkı sahibinin adı, adın tanınabileceği bir kısaltma veya kötü şöhretli alternatif bir sahip tanımı';
+$lang['descr_no-license'] = 'Size bu web sitesindeki herhangi bir materyali herhangi bir şekilde yeniden kullanma hakkı veren bir lisans yoktur, telif haklarına bakın. “Her Hakkı Saklıdır” formülünün hiçbir yargı alanında yasal bir değeri kalmadığını, ancak burada belirsizliği önlemek için kullanıldığını unutmayın.';
+$lang['descr_cc'] = 'Çalışmayı [KOPYALAMA], [DEĞİŞTİRME], [DAĞITMA] ve [GERÇEKLEŞTİRME], [TİCARİ] amaçlar için bile, izin almadan yapabilirsiniz.';
+$lang['descr_by-sa'] = '[PAYLAŞMAK] (herhangi bir ortamda veya formatta materyali kopyalayıp yeniden dağıtmak) veya [ADAPT] (yeniden karıştırmak, dönüştürmek ve materyali üzerine inşa etmek) için [TİCARİ OLARAK] da olsa özgürsünüz ancak uygun [KREDİ] vermelisiniz. Malzemeyi yeniden düzenler, dönüştürür veya malzeme üzerine inşa ederseniz, katkılarınızı orijinaliyle [AYNI LİSANS ALTINDA] dağıtmanız gerekir.';
+$lang['descr_by-nd'] = '[TİCARİ OLARAK] da olsa, herhangi bir amaçla [PAYLAŞMAK] (herhangi bir ortamda veya formatta materyali kopyalamak ve yeniden dağıtmak) konusunda özgürsünüz. Malzemeyi yeniden karıştırır, dönüştürür veya üzerine inşa ederseniz, [DEĞİŞTİRİLEN MALZEMEYİ DAĞITILAMAZSINIZ].';
+$lang['descr_by-nc-sa'] = 'Herhangi bir [TİCARİ OLMAYAN] amaç için [PAYLAŞMAK] (herhangi bir ortamda veya formatta materyali kopyalayıp yeniden dağıtmak) veya [ADAPT] (yeniden karıştırmak, dönüştürmek ve materyal üzerine inşa etmek) yapmakta özgürsünüz ancak uygun [KREDİ] vermeniz gerekir. Malzemeyi yeniden düzenler, dönüştürür veya malzeme üzerine inşa ederseniz, katkılarınızı orijinaliyle [AYNI LİSANS ALTINDA] dağıtmanız gerekir.';
+$lang['descr_by-nc-nd'] = 'Herhangi bir [TİCARİ OLMAYAN] amaç için [PAYLAŞMAK] (herhangi bir ortamda veya formatta materyali kopyalamak ve yeniden dağıtmak) konusunda özgürsünüz ancak uygun [KREDİ] vermelisiniz. Malzemeyi yeniden karıştırır, dönüştürür veya üzerine inşa ederseniz, [DEĞİŞTİRİLEN MALZEMEYİ DAĞITILAMAZSINIZ].';
+$lang['descr_by-nc'] = 'Herhangi bir [TİCARİ OLMAYAN] amaç için [PAYLAŞMAK] (herhangi bir ortamda veya formatta materyali kopyalayıp yeniden dağıtmak) veya [ADAPT] (yeniden karıştırmak, dönüştürmek ve materyal üzerine inşa etmek) yapmakta özgürsünüz ancak uygun [KREDİ] vermeniz gerekir.';
+$lang['descr_by'] = '[PAYLAŞMA] (herhangi bir ortamda veya formatta materyali kopyalayıp yeniden dağıtmak) veya [ADAPTE ETMEK] (yeniden karıştırmak, dönüştürmek ve materyali üzerine inşa etmek) için [TİCARİ OLARAK] da olsa özgürsünüz ancak uygun [KREDİ] vermelisiniz.';
+$lang['Will be shown as a tooltip when hovering license link'] = 'Lisans bağlantısının üzerine gelindiğinde bir araç ipucu olarak gösterilecek';
+$lang['Use original image URL instead of page URL for link\'s `about` property'] = 'Bağlantının "hakkında" özelliği için sayfa URL\'si yerine orijinal resim URL\'sini kullanın';
+$lang['Turn recognizable license label in Copyright metadata into a link to that license details'] = 'Telif hakkı meta verilerindeki tanınabilir lisans etiketini bu lisans ayrıntılarına giden bir bağlantıya dönüştürün';
+$lang['This option seems to greatly improve images SEO but keep in mind that it offers a link to download original image.'] = 'Bu seçenek görsel SEO\'yu büyük ölçüde geliştiriyor gibi görünse de, orijinal görseli indirmek için bir bağlantı sunduğunu unutmayın.';
+$lang['Note that the abbreviation might not be acceptable in some countries'] = 'Kısaltmanın bazı ülkelerde kabul edilmeyebileceğini unutmayın.';
+$lang['Note that displaying a copyright and/or a license on a web page isn\'t as legally efficient as adding them in picture\'s metadatas, it only makes them more visible.'] = 'Bir web sayfasında bir telif hakkı ve/veya lisans görüntülemenin, bunları resmin meta verilerine eklemek kadar verimli olmadığını, yalnızca daha görünür hale getirdiğini unutmayın.';
+$lang['label_cc'] = 'CC0 1.0 Evrensel';
+$lang['url_no-license'] = 'https://tr.wikipedia.org/wiki/T%C3%BCm_haklar%C4%B1_sakl%C4%B1d%C4%B1r';
+$lang['url_cc'] = 'https://creativecommons.org/publicdomain/zero/1.0/deed.tr';
+$lang['url_by-sa'] = 'https://creativecommons.org/licenses/by-sa/4.0/deed.tr';
+$lang['url_by-nd'] = 'https://creativecommons.org/licenses/by-nd/4.0/deed.tr';
+$lang['url_by-nc-sa'] = 'https://creativecommons.org/licenses/by-nc-sa/4.0/deed.tr';
+$lang['url_by-nc-nd'] = 'https://creativecommons.org/licenses/by-nc-nd/4.0/deed.tr';
+$lang['url_by'] = 'https://creativecommons.org/licenses/by/4.0/deed.tr';
+$lang['url_by-nc'] = 'https://creativecommons.org/licenses/by-nc/4.0/deed.tr';
+$lang['see below'] = 'aşağıya bakınız';
+$lang['label_no-license'] = 'Tüm Hakları Saklıdır';
+$lang['the year of first publication'] = 'ilk yayın yılı';
+$lang['this Wikipedia article'] = 'bu Wikipedia makalesi';
+$lang['the © symbol (C letter in a circle), the word “Copyright”, or, the abbreviation “Copr.”'] = '© sembolü (daire içinde C harfi), “Telif hakkı” kelimesi veya “Telif” kısaltması.';
+$lang['label_by-sa'] = 'CC Atıf-AynıLisanslaPaylaş 4.0 Uluslararası';
+$lang['label_by-nd'] = 'CC Alıntı-Türetilemez 4.0 Uluslararası';
+$lang['label_by-nc-sa'] = 'CC Atıf-GayriTicari-AynıLisanslaPaylaş 4.0 Uluslararası';
+$lang['label_by-nc-nd'] = 'CC Alıntı-GayriTicari-Türetilemez 4.0 Uluslararası';
+$lang['label_by-nc'] = 'CC Atıf-GayriTicari 4.0 Uluslararası';
+$lang['label_by'] = 'CC Atıf 4.0 Uluslararası';
+$lang['URL:'] = 'URL:';
+$lang['Summary:'] = 'Özet:';
+$lang['Short description:'] = 'Kısa açıklama:';
+$lang['License'] = 'Lisans';
+$lang['No license'] = 'Lisans yok';
+$lang['Image pages'] = 'Görüntü sayfaları';
+$lang['It is possible and often advised (or even required in some countries) to add the licence after the Copyright, here come a few examples:'] = 'Lisansın Telif Hakkı\'ndan sonra eklenmesi mümkündür ve genellikle tavsiye edilir (hatta bazı ülkelerde zorunludur), işte birkaç örnek:';
+$lang['Label:'] = 'Etiket:';
+$lang['Image does not contain any Copyright, it would be wise to add one in metadata.'] = 'Görüntü herhangi bir Telif Hakkı içermiyor, meta verilere bir tane eklemek akıllıca olacaktır.';
+$lang['If image has no Copyright info, display default license'] = 'Görüntünün Telif hakkı bilgisi yoksa, varsayılan lisansı göster';
+$lang['Excerpt from'] = 'Alıntı';
+$lang['Except where otherwise noted, there is no license granted on this website\'s content, which means it is protected by corresponding copyright'] = 'Aksi belirtilmedikçe, bu web sitesinin içeriğine herhangi bir lisans verilmemiştir, bu da ilgili telif hakkıyla korunduğu anlamına gelir';
+$lang['Except where otherwise noted, content on this website is licensed under the following license:'] = 'Aksi belirtilmedikçe, bu web sitesindeki içerik aşağıdaki lisans kapsamında lisanslanmıştır:';
+$lang['Elements legally required to correctly present a Copyright in most countries:'] = 'Çoğu ülkede bir Telif Hakkını doğru bir şekilde sunmak için yasal olarak gerekli olan unsurlar:';
+$lang['Examples of recognizable license labels:'] = 'Tanınabilir lisans etiketi örnekleri:';
+$lang['Display default license in site footer'] = 'Site alt bilgisinde varsayılan lisansı göster';
+$lang['Any license requires a link to a full description to be of any value'] = 'Herhangi bir lisans, herhangi bir değere sahip olmak için tam bir açıklamaya bağlantı gerektirir';
+$lang['Custom license label shown by Piwigo'] = 'Piwigo tarafından gösterilen özel lisans etiketi';
+$lang['Custom license'] = 'Özel lisans';
+$lang['Display image page Copyright section'] = 'Görüntü sayfasının telif hakkı bölümünü görüntüle';
+$lang['Default license'] = 'Varsayılan lisans';
+$lang['Copyright'] = 'Telif hakkı';
+$lang['About copyright notice'] = 'Telif hakkı uyarısı hakkında';
+$lang['Choice:'] = 'Seçim:';
+$lang['All Rights Reserved'] = 'Tüm Hakları Saklıdır';
