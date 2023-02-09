7bd7a739b923f43aecb01fc1a327d4efcb546eff
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-02-09 08:50:44 +0100

    Update es_ES, thanks to Tirs

--- language/es_ES/plugin.lang.php
+++ language/es_ES/plugin.lang.php
@@ -6,3 +6,17 @@
 // | file that was distributed with this source code.                      |
 // +-----------------------------------------------------------------------+
 $lang['Copyright'] = 'Copyright';
+$lang['About copyright notice'] = 'Acerca de la nota de copyright';
+$lang['All Rights Reserved'] = 'Reservados todos los derechos';
+$lang['Any license requires a link to a full description to be of any value'] = 'Toda licencia requiere un enlace a una descripción completa para tener valor';
+$lang['Choice:'] = 'Selección:';
+$lang['Custom license'] = 'Licencia personalizada';
+$lang['Custom license label shown by Piwigo'] = 'Etiqueta de licencia personalizada mostrada por Piwigo';
+$lang['Default license'] = 'Licencia por defecto';
+$lang['Display default license in site footer'] = 'Mostrar la licencia por defecto al pie del sitio';
+$lang['Display image page Copyright section'] = 'Mostrar la sección de Copyright de la página de imágenes';
+$lang['Elements legally required to correctly present a Copyright in most countries:'] = 'Elementos requeridos legalmente para presentar correctamente una página de Copyright en la mayoría de países:';
+$lang['Examples of recognizable license labels:'] = 'Ejemplos de etiquetas de licencia reconocibles:';
+$lang['Except where otherwise noted, content on this website is licensed under the following license:'] = 'Excepto donde se indique lo contrario, el contenido de este sitio web está bajo la licencia siguiente:';
+$lang['Except where otherwise noted, there is no license granted on this website\'s content, which means it is protected by corresponding copyright'] = 'Excepto donde se indique lo contrario, no se ha concedido ninguna licencia sobre el contenido de este sitio web, lo que significa que está protegido por el correspondiente copyright';
+$lang['Excerpt from'] = 'Extracto de';
