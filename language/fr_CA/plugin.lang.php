e8347011e2ceacca30575e5a6a6e4f9f4e51cd0b
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-02-09 08:50:39 +0100

    Update fr_CA, thanks to bgoutier

---
+++ language/fr_CA/plugin.lang.php
@@ -0,0 +1,9 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Custom license label shown by Piwigo'] = 'Étiquette montrée par Piwigo pour la licence personnalisée';
+$lang['Label:'] = 'Étiquette :';
