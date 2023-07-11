df5829e351774fd13a27d89cf67b2b4ff6bd660c
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-07-11 13:59:01 +0200

    Update it_IT, thanks to kinmen

---
+++ language/it_IT/plugin.lang.php
@@ -0,0 +1,27 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['About copyright notice'] = 'Circa la notifica di diritto d\'autore.';
+$lang['All Rights Reserved'] = 'Tutti i diritti riservati.';
+$lang['Any license requires a link to a full description to be of any value'] = 'Qualunque licenza per essere valida richiede un collegamento alla sua completa descrizione.';
+$lang['Choice:'] = 'Scelta:';
+$lang['Copyright'] = 'Diritto d\'autore.';
+$lang['Custom license'] = 'Licenza personalizzata.';
+$lang['Custom license label shown by Piwigo'] = 'Etichetta della licenza personalizzata mostrata da Piwigo.';
+$lang['Default license'] = 'Licenza preimpostata.';
+$lang['Display default license in site footer'] = 'Mostra la licenza preimpostata nel piè di pagina del sito.';
+$lang['Display image page Copyright section'] = 'Visualizza la sezione Copyright nella pagina dell\'immagine.';
+$lang['Elements legally required to correctly present a Copyright in most countries:'] = 'Elementi legalmente vincolanti per mostrare correttamente il diritto d\'autore nella maggior parte delle nazioni:';
+$lang['Examples of recognizable license labels:'] = 'Esempio di etichette di licenze riconoscibili:';
+$lang['Except where otherwise noted, content on this website is licensed under the following license:'] = 'Ad eccezione di dove diversamente annotato, il contenuto di questo sito web è fruibile secondo la seguente licenza:';
+$lang['Except where otherwise noted, there is no license granted on this website\'s content, which means it is protected by corresponding copyright'] = 'Ad eccezione di dove diversamente annotato, nessuna licenza è concessa sul contenuto di questo sito, il che significa che esso è protetto dal corrispondente diritto d\'autore.';
+$lang['Excerpt from'] = 'Eccetto da';
+$lang['If image has no Copyright info, display default license'] = 'Se all\'immagine non è associata alcuna licenza d\'uso, mostra quella preimpostata.';
+$lang['Image does not contain any Copyright, it would be wise to add one in metadata.'] = 'L\'immagine non contiene alcuna licenza d\'uso, sarebbe saggio includerne una nei metadati.';
+$lang['Image pages'] = 'Pagine delle immagini.';
+$lang['It is possible and often advised (or even required in some countries) to add the licence after the Copyright, here come a few examples:'] = 'È possibile, e spesso consigliabile, (od addirittura obbligatorio in alcune nazioni) far seguire al diritto d\'autore la corrispondente licenza, come mostriamo di seguito con alcuni esempi:';
+$lang['Label:'] = 'Etichetta:';
