4c40fe521167b739f54d593d33b1538d9e75dfe8
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-10-26 06:36:52 +0200

    Update ru_RU, thanks to rurio

---
+++ language/ru_RU/plugin.lang.php
@@ -0,0 +1,29 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['All Rights Reserved'] = 'Все права защищены';
+$lang['Short description:'] = 'Короткое описание:';
+$lang['URL:'] = 'URL:';
+$lang['label_by'] = 'CC Attribution 4.0 International';
+$lang['label_by-nc'] = 'CC Attribution-NonCommercial 4.0 International';
+$lang['label_by-nc-nd'] = 'CC Attribution-NonCommercial-NoDerivatives 4.0 International';
+$lang['label_by-nc-sa'] = 'CC Attribution-NonCommercial-ShareAlike 4.0 International';
+$lang['label_by-nd'] = 'CC Attribution-NoDerivatives 4.0 International';
+$lang['label_by-sa'] = 'CC Attribution-ShareAlike 4.0 International';
+$lang['label_cc'] = 'CC0 1.0 Universal';
+$lang['label_no-license'] = 'Все права защищены';
+$lang['see below'] = 'смотрите ниже';
+$lang['the year of first publication'] = 'год первой публикации';
+$lang['this Wikipedia article'] = 'статья на Википедии';
+$lang['url_by'] = 'https://creativecommons.org/licenses/by/4.0/';
+$lang['url_by-nc'] = 'https://creativecommons.org/licenses/by-nc/4.0/';
+$lang['url_by-nc-nd'] = 'https://creativecommons.org/licenses/by-nc-nd/4.0/';
+$lang['url_by-nc-sa'] = 'https://creativecommons.org/licenses/by-nc-sa/4.0/';
+$lang['url_by-nd'] = 'https://creativecommons.org/licenses/by-nd/4.0/';
+$lang['url_by-sa'] = 'https://creativecommons.org/licenses/by-sa/4.0/';
+$lang['url_cc'] = 'https://creativecommons.org/publicdomain/zero/1.0/deed.ru';
+$lang['url_no-license'] = 'https://ru.wikipedia.org/wiki/All_rights_reserved';
