b60ec0f8c380a2c6671626ff4b8751f3d1620a73
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-11-08 14:48:51 +0100

    Update zh_CN, thanks to walle

---
+++ language/zh_CN/plugin.lang.php
@@ -0,0 +1,68 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['label_by'] = 'CC署名 4.0 国际';
+$lang['label_by-nc'] = 'CC署名-非商业性使用 4.0 国际';
+$lang['label_by-nc-nd'] = 'CC署名-非商业性使用-禁止演绎 4.0 国际';
+$lang['label_by-nc-sa'] = 'CC署名-非商业性使用-相同方式共享 4.0 国际';
+$lang['label_by-nd'] = 'CC署名-禁止演绎 4.0 国际';
+$lang['label_by-sa'] = 'CC署名-相同方式共享 4.0 国际';
+$lang['label_cc'] = 'CC0 1.0 通用';
+$lang['label_no-license'] = '版权所有';
+$lang['see below'] = '见下文';
+$lang['the name of the copyright owner, an abbreviation by which the name can be recognized, or a notoriously known alternative designation of owner'] = '版权拥有者的名称、可以识别名称的缩写或公认的替代名称';
+$lang['the year of first publication'] = '首次出版年份';
+$lang['the © symbol (C letter in a circle), the word “Copyright”, or, the abbreviation “Copr.”'] = '© 符号（一个圆圈中的C字母）、“版权”一词或“Copr.”的缩写';
+$lang['this Wikipedia article'] = '这篇维基百科文章';
+$lang['url_no-license'] = 'https://en.wikipedia.org/wiki/All_rights_reserved';
+$lang['url_by'] = 'https://creativecommons.org/licenses/by/4.0/';
+$lang['url_by-nc'] = 'https://creativecommons.org/licenses/by-nc/4.0/';
+$lang['url_by-nc-nd'] = 'https://creativecommons.org/licenses/by-nc-nd/4.0/';
+$lang['url_by-nc-sa'] = 'https://creativecommons.org/licenses/by-nc-sa/4.0/';
+$lang['url_by-nd'] = 'https://creativecommons.org/licenses/by-nd/4.0/';
+$lang['url_by-sa'] = 'https://creativecommons.org/licenses/by-sa/4.0/';
+$lang['url_cc'] = '
+https://creativecommons.org/publicdomain/zero/1.0/deed.en';
+$lang['descr_by'] = '您可以自由地[共享]（在任何媒介或格式中复制和分发材料）或[改编]（修改、转换和基于材料进行创作）用于任何目的，即使是[商业]用途，但必须给予适当的[署名]。';
+$lang['descr_by-nc'] = '您可以自由地[共享]（在任何媒介或格式中复制和分发材料）或[改编]（修改、转换和基于材料进行创作）用于任何[非商业]目的，但必须给予适当的[署名]。';
+$lang['descr_by-nc-nd'] = '您可以自由地[共享]（在任何媒介或格式中复制和分发材料）用于任何[非商业]目的，但必须给予适当的[署名]。如果您修改、转换或基于材料进行创作，您[不得分发修改后的材料]。';
+$lang['descr_by-nc-sa'] = '您可以自由地[共享]（在任何媒介或格式中复制和分发材料）或[改编]（修改、转换和基于材料进行创作）用于任何[非商业]目的，但必须给予适当的[署名]。如果您修改、转换或基于材料进行创作，您必须在相同的许可证下分发您的贡献。';
+$lang['descr_by-nd'] = '您可以自由地[共享]（在任何媒介或格式中复制和分发材料）用于任何目的，即使是[商业]用途。如果您修改、转换或基于材料进行创作，您[不得分发修改后的材料]。';
+$lang['descr_by-sa'] = '您可以自由地[共享]（在任何媒介或格式中复制和分发材料）或[改编]（修改、转换和基于材料进行创作）用于任何目的，即使是[商业]用途，但必须给予适当的[署名]。如果您修改、转换或基于材料进行创作，您必须在相同的许可证下分发您的贡献。';
+$lang['descr_cc'] = '您可以[复制]、[修改]、[分发]和[表演]作品，即使是用于[商业]目的，所有这些都无需获得许可。';
+$lang['descr_no-license'] = '没有任何许可证授予您以任何方式重新使用本网站上的任何材料的权利，请参阅版权。请注意，“保留所有权利”公式在任何司法管辖区都没有任何法律效力，但在此使用是为了避免歧义。';
+$lang['License'] = '许可证';
+$lang['No license'] = '无许可证';
+$lang['Note that displaying a copyright and/or a license on a web page isn\'t as legally efficient as adding them in picture\'s metadatas, it only makes them more visible.'] = '请注意，在网页上显示版权和/或许可证不如将它们添加到图片的元数据中有效，这会使它们更明显。';
+$lang['Note that the abbreviation might not be acceptable in some countries'] = '请注意，某些国家/地区可能不接受缩写';
+$lang['Short description:'] = '简短描述：';
+$lang['Summary:'] = '摘要：';
+$lang['This option seems to greatly improve images SEO but keep in mind that it offers a link to download original image.'] = '此选项似乎大大改善了图像的SEO，但请记住，它提供了下载原始图像的链接。';
+$lang['Turn recognizable license label in Copyright metadata into a link to that license details'] = '将版权元数据中可识别的许可证标签转换为该许可证详细信息的链接。';
+$lang['URL:'] = 'url:';
+$lang['Use original image URL instead of page URL for link\'s `about` property'] = '使用原始图像URL而不是页面URL作为链接的about属性。';
+$lang['Will be shown as a tooltip when hovering license link'] = '当鼠标悬停在许可证链接上时，将显示为工具提示。';
+$lang['About copyright notice'] = '关于版权声明';
+$lang['All Rights Reserved'] = '版权所有';
+$lang['Any license requires a link to a full description to be of any value'] = '任何许可证都需要完整描述的链接才有价值';
+$lang['Choice:'] = '选择：';
+$lang['Copyright'] = '版权';
+$lang['Custom license'] = '自定义许可证';
+$lang['Custom license label shown by Piwigo'] = '显示自定义的许可证标签';
+$lang['Default license'] = '默认许可证';
+$lang['Display default license in site footer'] = '在网站页脚显示默认许可证';
+$lang['Display image page Copyright section'] = '显示图片页面的版权部分';
+$lang['Elements legally required to correctly present a Copyright in most countries:'] = '在大多数国家/地区正确呈现版权所需的法律要素：';
+$lang['Examples of recognizable license labels:'] = '可识别的许可证标签示例：';
+$lang['Except where otherwise noted, content on this website is licensed under the following license:'] = '除非另有说明，否则本网站内容受以下许可证保护：';
+$lang['Except where otherwise noted, there is no license granted on this website\'s content, which means it is protected by corresponding copyright'] = '除非另有说明，否则本网站内容未授予任何许可证，这意味着它受相应版权保护';
+$lang['Excerpt from'] = '摘录自';
+$lang['If image has no Copyright info, display default license'] = '如果图片没有版权信息，则显示默认许可证';
+$lang['Image does not contain any Copyright, it would be wise to add one in metadata.'] = '图片不包含任何版权信息，建议在元数据中添加一个。';
+$lang['Image pages'] = '图片页面';
+$lang['It is possible and often advised (or even required in some countries) to add the licence after the Copyright, here come a few examples:'] = '在版权之后添加许可证是可能的，而且通常建议这样做（在某些国家甚至要求这样做），下面是几个示例：';
+$lang['Label:'] = '标签：';
