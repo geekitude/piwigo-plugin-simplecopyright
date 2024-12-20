94c33ce9603cedb520fb7cb7b77d7c7e6a3000ab
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-12-20 17:56:52 +0100

    Update he_IL, thanks to epifeny

---
+++ language/he_IL/plugin.lang.php
@@ -0,0 +1,67 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['About copyright notice'] = 'אודות הודעת זכויות יוצרים';
+$lang['All Rights Reserved'] = 'כל הזכויות שמורות';
+$lang['Any license requires a link to a full description to be of any value'] = 'כל רישיון מחייב קישור לתיאור מלא כדי להיות שווה';
+$lang['Choice:'] = 'בחירה:';
+$lang['Copyright'] = 'זכויות יוצרים';
+$lang['Custom license'] = 'רישיון מותאם אישית';
+$lang['Custom license label shown by Piwigo'] = 'תווית רישיון מותאמת אישית המוצגת על ידי Piwigo';
+$lang['Default license'] = 'רישיון ברירת מחדל';
+$lang['Display default license in site footer'] = 'הצג רישיון ברירת מחדל בתחתית האתר';
+$lang['Display image page Copyright section'] = 'הצג את חלק הזכויות יוצרים בדף התמונה';
+$lang['Elements legally required to correctly present a Copyright in most countries:'] = 'אלמנטים הנדרשים מבחינה משפטית להצגת זכויות יוצרים תקינה ברוב המדינות:';
+$lang['Examples of recognizable license labels:'] = 'דוגמאות לתוויות רישיון מוכרות:';
+$lang['Except where otherwise noted, content on this website is licensed under the following license:'] = 'למעט במקום אחר, התוכן באתר זה רשות תחת הרישיון הבא:';
+$lang['Except where otherwise noted, there is no license granted on this website\'s content, which means it is protected by corresponding copyright'] = 'למעט במקום אחר, לא ניתן רישיון על תוכן האתר הזה, מה שאומר שהוא מוגן על ידי זכויות יוצרים תואמות';
+$lang['Excerpt from'] = 'ציטוט מתוך';
+$lang['If image has no Copyright info, display default license'] = 'אם אין בתמונה מידע על זכויות יוצרים, הצג רישיון ברירת מחדל';
+$lang['Image does not contain any Copyright, it would be wise to add one in metadata.'] = 'בדפי התמונות אין שום זכויות יוצרים, כדאי להוסיף אחת במטא-נתונים.';
+$lang['Image pages'] = 'דפי התמונות';
+$lang['It is possible and often advised (or even required in some countries) to add the licence after the Copyright, here come a few examples:'] = 'ניתן ונמלץ (או אף דרוש בכמה מדינות) להוסיף את הרישיון אחרי הזכויות יוצרים, הנה מספר דוגמאות:';
+$lang['Label:'] = 'תווית:';
+$lang['License'] = 'רישיון';
+$lang['No license'] = 'ללא רישיון';
+$lang['Note that displaying a copyright and/or a license on a web page isn\'t as legally efficient as adding them in picture\'s metadatas, it only makes them more visible.'] = 'שימו לב שהצגת זכויות יוצרים ו/או רישיון בדף אינה כה יעילה כהוספתם במטא-נתונים של התמונה, זה רק מבליט אותם יותר.';
+$lang['Note that the abbreviation might not be acceptable in some countries'] = 'שימו לב שהראשיות עשויות לא להיות מקובלות בכמה מדינות';
+$lang['Short description:'] = 'תיאור קצר:';
+$lang['Summary:'] = 'סיכום:';
+$lang['This option seems to greatly improve images SEO but keep in mind that it offers a link to download original image.'] = 'אפשרות זו נראית כי משפרת מאוד את SEO של התמונות אך יש לזכור שהיא מציעה קישור להורדת התמונה המקורית.';
+$lang['Turn recognizable license label in Copyright metadata into a link to that license details'] = 'הפוך את תווית הרישיון המוכרת במטא-נתונים של הזכויות יוצרים לקישור לפרטי הרישיון ההם';
+$lang['URL:'] = 'כתובת URL:';
+$lang['Use original image URL instead of page URL for link\'s `about` property'] = 'השתמש בכתובת URL של התמונה המקורית במקום בכתובת הדף למאפיין `about` של הקישור';
+$lang['Will be shown as a tooltip when hovering license link'] = 'יוצג כטקסט עזר כאשר מעבירים על קישור רישיון';
+$lang['descr_by'] = 'אתה רשאי ל[לשתף] (להעתיק ולהפיץ את החומר בכל תושבת או פורמט) או [להתאים] (לערבב, להמיר ולבנות על החומר) לכל מטרה, אפילו [מסחרית] אך עליך לתת [קרדיט] ראוי.';
+$lang['descr_by-nc'] = 'אתה רשאי ל[לשתף] (להעתיק ולהפיץ את החומר בכל תושבת או פורמט) או [להתאים] (לערבב, להמיר ולבנות על החומר) לכל מטרה [לא-מסחרית] אך עליך לתת [קרדיט] ראוי.';
+$lang['descr_by-nc-nd'] = 'אתה רשאי ל[לשתף] (להעתיק ולהפיץ את החומר בכל תושבת או פורמט) לכל מטרה [לא-מסחרית] אך עליך לתת [קרדיט] ראוי. אם אתה מערבב, משנה או בונה על החומר, אינך רשאי להפיץ את החומר המשונה.';
+$lang['descr_by-nc-sa'] = 'אתה רשאי ל[לשתף] (להעתיק ולהפיץ את החומר בכל תושבת או פורמט) או [להתאים] (לערבב, להמיר ולבנות על החומר) לכל מטרה [לא-מסחרית] אך עליך לתת [קרדיט] ראוי. אם אתה מערבב, משנה או בונה על החומר, עליך להפיץ את התרומות שלך [בפיות הרישיון הזה] כמו המקור.';
+$lang['descr_by-nd'] = 'אתה רשאי ל[לשתף] (להעתיק ולהפיץ את החומר בכל תושבת או פורמט) לכל מטרה, אפילו [מסחרית]. אם אתה מערבב, משנה או בונה על החומר, אינך רשאי להפיץ את החומר המשונה.';
+$lang['descr_by-sa'] = 'אתה רשאי ל[לשתף] (להעתיק ולהפיץ את החומר בכל תושבת או פורמט) או [להתאים] (לערבב, להמיר ולבנות על החומר) לכל מטרה, אפילו [מסחרית] אך עליך לתת [קרדיט] ראוי. אם אתה מערבב, משנה או בונה על החומר, עליך להפיץ את התרומות שלך [בפיות הרישיון הזה] כמו המקור.';
+$lang['descr_cc'] = 'אתה רשאי ל[להעתיק], [לשנות], [להפיץ] ו[לבצע] את העבודה, אפילו למטרות [מסחריות], הכל בלי לבקש רשות.';
+$lang['descr_no-license'] = 'אין רישיון המעניק לך זכות לשימוש מחדש בכל חומר מהאתר הזה בכל דרך, יש להתייחס לזכויות יוצרים. שימו לב שהנוסח "כל הזכויות שמורות" אינו מחזיק ערך משפטי בשום רשות אך משמש כאן למניעת סמיכות.';
+$lang['label_by'] = 'CC ייחודי 4.0 בינלאומי';
+$lang['label_by-nc'] = 'CC ייחודי-לא-מסחרי 4.0 בינלאומי';
+$lang['label_by-nc-nd'] = 'CC ייחודי-לא-מסחרי-ללא-שינויים 4.0 בינלאומי';
+$lang['label_by-nc-sa'] = 'CC ייחודי-לא-מסחרי-שיתוף-עם-השקעה 4.0 בינלאומי';
+$lang['label_by-nd'] = 'CC ייחודי-ללא-שינויים 4.0 בינלאומי';
+$lang['label_by-sa'] = 'CC ייחודי-שיתוף-עם-השקעה 4.0 בינלאומי';
+$lang['label_cc'] = 'CC0 אוניברסלי 1.0';
+$lang['label_no-license'] = 'כל הזכויות שמורות';
+$lang['see below'] = 'ראה למטה';
+$lang['the name of the copyright owner, an abbreviation by which the name can be recognized, or a notoriously known alternative designation of owner'] = 'שם בעל זכויות היוצר, קיצור שבו ניתן לזהות את השם, או תיאור חלופי נודע של בעל הזכויות';
+$lang['the year of first publication'] = 'שנת הפרסום הראשונה';
+$lang['the © symbol (C letter in a circle), the word “Copyright”, or, the abbreviation “Copr.”'] = 'סמל © (אות C במעגל), המילה "זכויות יוצרים", או, הקיצור "Copr."';
+$lang['this Wikipedia article'] = 'המאמר בוויקיפדיה זו';
+$lang['url_by'] = 'https://creativecommons.org/licenses/by/4.0/';
+$lang['url_by-nc'] = 'https://creativecommons.org/licenses/by-nc/4.0/';
+$lang['url_by-nc-nd'] = 'https://creativecommons.org/licenses/by-nc-nd/4.0/';
+$lang['url_by-nc-sa'] = 'https://creativecommons.org/licenses/by-nc-sa/4.0/';
+$lang['url_by-nd'] = 'https://creativecommons.org/licenses/by-nd/4.0/';
+$lang['url_by-sa'] = 'https://creativecommons.org/licenses/by-sa/4.0/';
+$lang['url_cc'] = 'https://creativecommons.org/publicdomain/zero/1.0/deed.en';
+$lang['url_no-license'] = 'https://en.wikipedia.org/wiki/All_rights_reserved';
