2bff8a1845560fad9e2c20b11de172f836b62c15
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-03-03 12:35:19 +0100

    Update ko_KR, thanks to yjchoi

---
+++ language/ko_KR/plugin.lang.php
@@ -0,0 +1,67 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['label_by-nc'] = 'CC 저작자표시-비영리 4.0 국제';
+$lang['label_by-nc-nd'] = 'CC 저작자표시-비영리-변경금지 4.0 국제';
+$lang['label_by-nc-sa'] = 'CC 저작자표시-비영리-동일조건변경허락 4.0 국제';
+$lang['label_by-nd'] = 'CC 저작자표시-변경금지 4.0 국제';
+$lang['label_by-sa'] = 'CC 저작자표시-동일조건변경허락 4.0 국제';
+$lang['label_cc'] = 'CC0 1.0 유니버설';
+$lang['label_no-license'] = '모든 권리 보유';
+$lang['see below'] = '아래 참조';
+$lang['the name of the copyright owner, an abbreviation by which the name can be recognized, or a notoriously known alternative designation of owner'] = '저작권 소유자의 이름, 해당 이름을 식별할 수 있는 약어 또는 널리 알려진 대체 명칭';
+$lang['the year of first publication'] = '최초 출판 연도';
+$lang['the © symbol (C letter in a circle), the word “Copyright”, or, the abbreviation “Copr.”'] = 'ⓒ 기호(원 안에 C 글자), “Copyright”라는 단어 또는 “Copr.”라는 약어';
+$lang['this Wikipedia article'] = '이 위키백과 문서';
+$lang['url_by'] = 'https://creativecommons.org/licenses/by/4.0/';
+$lang['url_by-nc'] = 'https://creativecommons.org/licenses/by-nc/4.0/';
+$lang['url_by-nc-nd'] = 'https://creativecommons.org/licenses/by-nc-nd/4.0/';
+$lang['url_by-nc-sa'] = 'https://creativecommons.org/licenses/by-nc-sa/4.0/';
+$lang['url_by-nd'] = 'https://creativecommons.org/licenses/by-nd/4.0/';
+$lang['url_by-sa'] = 'https://creativecommons.org/licenses/by-sa/4.0/';
+$lang['url_cc'] = 'https://creativecommons.org/publicdomain/zero/1.0/deed.en';
+$lang['url_no-license'] = 'https://en.wikipedia.org/wiki/All_rights_reserved';
+$lang['License'] = '라이선스';
+$lang['No license'] = '라이선스 없음';
+$lang['Note that displaying a copyright and/or a license on a web page isn\'t as legally efficient as adding them in picture\'s metadatas, it only makes them more visible.'] = '웹 페이지에 저작권 및/또는 라이선스를 표시하는 것은 이미지 메타데이터에 추가하는 것만큼 효율적이지 않으며, 단지 가시성을 높일 뿐입니다.';
+$lang['Note that the abbreviation might not be acceptable in some countries'] = '일부 국가에서는 약어가 허용되지 않을 수 있습니다.';
+$lang['Short description:'] = '간략한 설명:';
+$lang['Summary:'] = '요약:';
+$lang['This option seems to greatly improve images SEO but keep in mind that it offers a link to download original image.'] = '이 옵션은 이미지 SEO를 크게 향상시키는 것으로 보이지만, 원본 이미지 다운로드 링크를 제공한다는 점을 유의하십시오.';
+$lang['Turn recognizable license label in Copyright metadata into a link to that license details'] = '저작권 메타데이터 내 인식 가능한 라이선스 라벨을 해당 라이선스 상세 정보 링크로 변환';
+$lang['URL:'] = 'URL:';
+$lang['Use original image URL instead of page URL for link\'s `about` property'] = '링크의 `about` 속성에 페이지 URL 대신 원본 이미지 URL 사용';
+$lang['Will be shown as a tooltip when hovering license link'] = '라이선스 링크에 마우스를 올렸을 때 툴팁으로 표시됨';
+$lang['descr_by'] = '귀하는 [공유] (모든 매체나 형식으로 자료를 복사 및 재배포)하거나 [수정] (자료를 리믹스, 변형 및 발전)할 수 있으며, [상업적] 목적을 포함해 어떤 목적으로도 자유롭게 사용할 수 있으나 적절한 [출처 표시]를 해야 합니다.';
+$lang['descr_by-nc'] = '[비상업적] 목적으로는 자유롭게 [공유](모든 매체나 형식으로 자료를 복제 및 재배포)하거나 [수정](리믹스, 변형, 자료 기반 작업)할 수 있으나 적절한 [출처 표시]를 해야 합니다.';
+$lang['descr_by-nc-nd'] = '[비상업적] 목적으로는 자유롭게 [공유] (어떤 매체나 형식으로든 자료를 복제 및 재배포)할 수 있으나 적절한 [출처 표시]를 해야 합니다. 자료를 리믹스, 변형 또는 발전시키는 경우 [수정된 자료를 배포할 수 없습니다].';
+$lang['descr_by-nc-sa'] = '귀하는 [비영리] 목적으로 자유롭게 자료를 [공유] (어떤 매체나 형식으로든 복제 및 재배포)하거나 [변경] (리믹스, 변형 및 자료를 바탕으로 새로운 작품을 제작)할 수 있으나, 적절한 [출처 표시]를 해야 합니다. 리믹스, 변형 또는 자료를 기반으로 제작한 경우, 귀하의 기여물은 원본과 [동일한 라이선스]로 배포해야 합니다.';
+$lang['descr_by-nd'] = '귀하는 [상업적] 목적을 포함한 모든 목적으로 자료를 [공유](모든 매체 또는 형식으로 복사 및 재배포)할 수 있습니다. 자료를 리믹스, 변형 또는 발전시키는 경우, [수정된 자료를 배포할 수 없습니다].';
+$lang['descr_by-sa'] = '[공유](모든 매체나 형식으로 자료를 복사 및 재배포)하거나 [적응](리믹스, 변형 및 자료 발전)하는 것은 [상업적] 목적을 포함해 어떤 목적으로든 자유롭지만, 적절한 [출처 표시]를 해야 합니다. 자료를 리믹스, 변형 또는 확장하는 경우, 귀하의 기여물은 원본과 동일한 라이선스 하에 배포해야 합니다.';
+$lang['descr_cc'] = '귀하는 허가 없이도 [상업적] 목적을 포함하여 저작물을 [복제], [수정], [배포] 및 [공연]할 수 있습니다.';
+$lang['descr_no-license'] = '본 웹사이트의 자료를 어떠한 방식으로든 재사용할 수 있는 권리를 부여하는 라이선스는 존재하지 않습니다. 저작권 관련 사항을 참조하십시오. “모든 권리 보유(All Rights Reserved)” 표기는 현재 어느 관할권에서도 법적 효력을 갖지 않으나, 여기서는 모호함을 방지하기 위해 사용됩니다.';
+$lang['label_by'] = 'CC 저작자표시 4.0 국제';
+$lang['About copyright notice'] = '저작권 고지 사항';
+$lang['All Rights Reserved'] = '모든 권리 보유';
+$lang['Any license requires a link to a full description to be of any value'] = '어떤 라이선스든 가치를 가지려면 전체 설명으로 연결되는 링크가 필요합니다';
+$lang['Choice:'] = '선택 사항:';
+$lang['Copyright'] = '저작권';
+$lang['Custom license'] = '사용자 정의 라이선스';
+$lang['Custom license label shown by Piwigo'] = 'Piwigo에서 표시하는 사용자 정의 라이선스 라벨';
+$lang['Default license'] = '기본 라이선스';
+$lang['Display default license in site footer'] = '사이트 바닥글에 기본 라이선스 표시';
+$lang['Display image page Copyright section'] = '이미지 페이지 저작권 섹션 표시';
+$lang['Elements legally required to correctly present a Copyright in most countries:'] = '대부분 국가에서 저작권을 올바르게 표시하기 위해 법적으로 요구되는 요소:';
+$lang['Examples of recognizable license labels:'] = '인식 가능한 라이선스 라벨 예시:';
+$lang['Except where otherwise noted, content on this website is licensed under the following license:'] = '별도로 명시되지 않는 한, 본 웹사이트의 콘텐츠는 다음 라이선스에 따라 제공됩니다:';
+$lang['Except where otherwise noted, there is no license granted on this website\'s content, which means it is protected by corresponding copyright'] = '별도로 명시되지 않는 한, 본 웹사이트의 콘텐츠에는 어떠한 라이선스도 부여되지 않으며, 이는 해당 저작권으로 보호됨을 의미합니다';
+$lang['Excerpt from'] = '발췌문';
+$lang['If image has no Copyright info, display default license'] = '이미지에 저작권 정보가 없는 경우, 기본 라이선스 표시';
+$lang['Image does not contain any Copyright, it would be wise to add one in metadata.'] = '이미지에 저작권 정보가 포함되어 있지 않다면, 메타데이터에 추가하는 것이 좋습니다.';
+$lang['Image pages'] = '이미지 페이지';
+$lang['It is possible and often advised (or even required in some countries) to add the licence after the Copyright, here come a few examples:'] = '저작권 표시 뒤에 라이선스를 추가하는 것이 가능하며 종종 권장됩니다(일부 국가에서는 필수). 다음은 몇 가지 예시입니다:';
+$lang['Label:'] = '라벨:';
